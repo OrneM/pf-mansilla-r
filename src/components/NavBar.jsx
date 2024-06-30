ba9a82425a87086f0d0e6e9100f88a4377d4ef72
Ornela <ornemans88@gmail.com>
2024-06-30 14:48:21 -0300

    Agregue componentes

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import Navitem from "./Navitem.jsx";
+
+function NavBAr() {
+
+    return (
+    <>
+    <ul>
+        <Navitem />
+        <li>About</li>
+        <li>Contact</li>
+    
+    
+    </ul>
+    </> 
+    )
+  };
+  
+  export default NavBAr;
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' //  Upper CamelCase para nombrar los componentes de react
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.createRoot(rootElement).render(
+ // <React.StrictMode>
+    <App /> /*componente de react*/
+ // </React.StrictMode>,
 )
